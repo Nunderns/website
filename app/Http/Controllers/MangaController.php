f1aa1d22faac9beac23000be531c0d3edf4bd4ab
Henri Okayama <henri.okayama@gmail.com>
2025-01-14 16:27:16 -0300

    feat: commit creating manga page

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -7,12 +7,32 @@ use Illuminate\Http\Request;
 
 class MangaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $mangas = Manga::with('chapters')
-            ->orderBy('title')
-            ->paginate(20);
-            
+        $sort = $request->get('sort', 'latest');
+
+        switch ($sort) {
+            case 'az':
+                $mangas = Manga::orderBy('title')->paginate(20);
+                break;
+            case 'approval':
+                $mangas = Manga::orderBy('approval_rating', 'desc')->paginate(20);
+                break;
+            case 'trend':
+                $mangas = Manga::orderBy('trend_score', 'desc')->paginate(20);
+                break;
+            case 'views':
+                $mangas = Manga::orderBy('views', 'desc')->paginate(20);
+                break;
+            case 'new':
+                $mangas = Manga::orderBy('created_at', 'desc')->paginate(20);
+                break;
+            case 'latest':
+            default:
+                $mangas = Manga::with('chapters')->orderBy('updated_at', 'desc')->paginate(20);
+                break;
+        }
+
         return view('mangas.index', compact('mangas'));
     }
 
@@ -21,6 +41,4 @@ class MangaController extends Controller
         $manga->load('chapters');
         return view('mangas.show', compact('manga'));
     }
-
-    // Outros métodos do CRUD...
 }
